a1b7d8d475fcdcc87c010be54e0480ac359a438a
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-08 21:27:37 +0600

    Form toggle done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
 function App() {
+  const [activeClass, setActiveClass] = useState('');
 
   return (
     <div className="wrapper">
-      <div className="container">
+      <div className={`container ${activeClass}`}>
         <Signup />
         <Login />
         <div className="overlay-container">
           <div className="overlay-left">
             <h1>Welcome Back</h1>
             <p>To keep connected with us please login with your personal info</p>
-            <button id="signIn" className="overlay_btn">Sign In</button>
+            <button onClick={() => setActiveClass('')} id="signIn" className="overlay_btn">Sign In</button>
           </div>
           <div className="overlay-right">
             <h1>Hello, Friend</h1>
             <p>Enter your personal details and start journey with us</p>
-            <button id="signUp" className="overlay_btn">Sign Up</button>
+            <button onClick={() => setActiveClass('right-panel-active')} id="signUp" className="overlay_btn">Sign Up</button>
           </div>
         </div>
       </div>
