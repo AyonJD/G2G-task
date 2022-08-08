e5328ed62564a32808ca23f76594a9d6e1f74706
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-08 21:21:52 +0600

    Login form added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,27 @@
+
 import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-           
+        <div class="sign-in-container">
+            <form>
+                <h1>Sign In</h1>
+                <div class="social-links">
+                    <div>
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    </div>
+                    <div>
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </div>
+                    <div>
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button class="form_btn">Sign In</button>
+            </form>
         </div>
     );
 };
