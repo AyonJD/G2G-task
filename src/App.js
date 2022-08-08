d3a731785b368df9433169ea2cf465515c720ea0
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-08 21:19:52 +0600

    Signup form done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useEffect } from 'react';
 import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
+
   return (
-    <div>
-      
+    <div className="wrapper">
+      <div className="container">
+        <Signup />
+        <Login />
+        <div className="overlay-container">
+          <div className="overlay-left">
+            <h1>Welcome Back</h1>
+            <p>To keep connected with us please login with your personal info</p>
+            <button id="signIn" className="overlay_btn">Sign In</button>
+          </div>
+          <div className="overlay-right">
+            <h1>Hello, Friend</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button id="signUp" className="overlay_btn">Sign Up</button>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
